12e820e0580f061389ddf64e7b93dd05ad58cefa
pramit-marattha <pramitmarattha5497@gmail.com>
2022-04-18 16:10:04 +0545

    Add atomic model docz project

---
+++ src/components/atoms/button/button.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { StyledButton } from "./styles";
+
+const Button = (props) => {
+  return <StyledButton {...props}>{props.children}</StyledButton>;
+};
+
+export default Button;
